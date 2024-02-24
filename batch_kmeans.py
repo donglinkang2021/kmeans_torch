9c00dd8720f28b5d73b5c5c2a646c83f69da1b47
Linkdom <donglinkang2021@163.com>
2024-02-24 21:35:32 +0800

    add result to batch kmeans

--- batch_kmeans.py
+++ batch_kmeans.py
@@ -34,3 +34,20 @@ for iteration in range(num_iterations):
 
 print("Codebooks:", codebooks.shape)  # (g_subvectors, v_clusters, vector_dim / g_subvectors)
 print("Codes:", codes.shape)  # (n_vectors, g_subvectors)
+
+"""output
+X_subvectors: torch.Size([16, 1000, 8])
+idx: torch.Size([10])
+Iteration 0 mean distance: 3.8903117179870605
+Iteration 1 mean distance: 3.2654647827148438
+Iteration 2 mean distance: 3.202737331390381
+Iteration 3 mean distance: 3.1900131702423096
+Iteration 4 mean distance: 3.189864158630371
+Iteration 5 mean distance: 3.1919443607330322
+Iteration 6 mean distance: 3.194202423095703
+Iteration 7 mean distance: 3.1963143348693848
+Iteration 8 mean distance: 3.1979026794433594
+Iteration 9 mean distance: 3.1993894577026367
+Codebooks: torch.Size([16, 10, 8])
+Codes: torch.Size([16, 1000])
+"""
